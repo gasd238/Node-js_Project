85135577f2b7360c77abbd3daf9575a9e9467b99
gasd238 <gasd238@naver.com>
2019-06-11 12:15:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ var app = express();
 var logger = require('morgan');
 
 app.use(logger('dev')); //로그를 남기는 미들웨어 실행
-app.use(express.static('public')); //정적파일 처리 미들웨어 실행
-app.use(express.urlencoded({extended:true})); //body로 넘어온 데이터 파싱
